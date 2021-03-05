9d2cc1ad3982b1c4d1ccae8cb3ad0723572faa55
JustinMartin7x <70045909+JustinMartin7x@users.noreply.github.com>
2021-03-05 14:04:06 -0800

    add user model, route, endpoint (#6)

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,9 +2,50 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
+const { executionAsyncId } = require('async_hooks');
+
+const justin = {
+  given_name: 'Justin',
+
+  family_name: 'Martin',
+
+  nickname: 'justin.martin7x',
+
+  name: 'Justin Martin',
+
+  picture:
+    'https://lh3.googleusercontent.com/a-/AOh14Gjihbg07z5ZK6RmDAjlOZAfaK1W4ZZ48cpLdMLRcsw=s96-c',
+
+  locale: 'en',
+
+  updated_at: '2021-03-03T19:43:35.603Z',
+
+  email: 'justin.martin7x@gmail.com',
+
+  email_verified: true,
+
+  sub: 'google-oauth2|116678650705106025315',
+};
 
 describe('m4thl33tz-server routes', () => {
   beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+  });
+
+  it('should create a new user using the information from AUTH0 using post route', async () => {
+    const response = await request(app)
+      .post('/newUser')
+      .send({
+        uniqueId: justin.sub,
+        name: justin.given_name + ' ' + justin.family_name,
+        nickname: justin.nickname,
+      });
+
+    expect(response.body).toEqual({
+      id: '1',
+      uniqueId: 'google-oauth2|116678650705106025315',
+      name: 'Justin Martin',
+      nickname: 'justin.martin7x',
+    });
   });
 });
